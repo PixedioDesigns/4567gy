7c8cf6fc869068f85332b13d006da90fe098230b
star-works <farukhkhan1115@gmail.com>
2022-12-09 00:23:25 +0530

    footer

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
+import Footer from "../components/home/Footer";
 import Hero from "../components/home/Hero";
 import OurCustomer from "../components/home/OurCustomer";
 import OurServices from "../components/home/OurServices";
 import Performance from "../components/home/Performance";
 
 export default function Home() {
-    return (
-        <>
-            <div>
-                <Hero />
-                <Performance />
-                <OurServices />
-                <OurCustomer />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Hero />
+      <Performance />
+      <OurServices />
+      <OurCustomer />
+      <Footer />
+    </>
+  );
 }
