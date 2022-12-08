f5e9161e0c096bf9d850934a9c4be466904e54c9
star-works <farukhkhan1115@gmail.com>
2022-12-08 22:28:22 +0530

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,55 +1,58 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {
-            fontSize: {
-                xs: "11.26px",
-                base: "12px",
-                sm: "14px",
-                "2sm": "15px",
-                md: "16px",
-                "2md": "17px",
-                lg: "18px",
-                xl: "20px",
-                "2xl": "22px",
-                "3xl": "25px",
-                "4xl": "30px",
-                "5xl": "35px",
-                "6xl": "45px",
-                "7xl": "50px",
-                "8xl": "56px",
-                "9xl": "60px",
-                xxl: "65px",
-                "2xxl": "75px",
-            },
-            colors: {
-                darkBlack: "#101828",
-                BrilliantLicorice: "#555252",
-                lakeThun: "#49C2E5",
-                dull: "#767171",
-                Madonna: "#3F4352",
-            },
-            screens: {
-                sm: "640px",
-                // => @media (min-width: 640px) { ... }
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        xs: "11.26px",
+        base: "12px",
+        sm: "14px",
+        "2sm": "15px",
+        md: "16px",
+        "2md": "17px",
+        lg: "18px",
+        xl: "20px",
+        "2xl": "22px",
+        "3xl": "25px",
+        "4xl": "30px",
+        "5xl": "35px",
+        "6xl": "45px",
+        "7xl": "50px",
+        "8xl": "56px",
+        "9xl": "60px",
+        "10xl": "65px",
+        "11xl": "75px",
+      },
+      colors: {
+        darkBlack: "#101828",
+        BrilliantLicorice: "#555252",
+        lakeThun: "#49C2E5",
+        dull: "#767171",
+        Madonna: "#3F4352",
+      },
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
 
-                md: "768px",
-                // => @media (min-width: 768px) { ... }
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
 
-                lg: "1024px",
-                // => @media (min-width: 1024px) { ... }
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
 
-                xl: "1280px",
-                // => @media (min-width: 1280px) { ... }
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
 
-                "2xl": "1536px",
-                // => @media (min-width: 1536px) { ... }
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
 
-                "3xl": "1700px",
-                // => @media (min-width: 1700px) { ... }
-            },
-        },
+        "3xl": "1700px",
+        // => @media (min-width: 1700px) { ... }
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
