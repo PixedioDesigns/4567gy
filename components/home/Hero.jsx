153631d308aebf77579b2446208cff6654a01e33
deepak9875 <deepaklakhlan1234@gmail.com>
2022-12-08 18:36:18 +0530

    add font size in tailwind css file

---
+++ components/home/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <div>
+            <div className="container mx-auto">
+                <div>
+                    <h1>Get your business online </h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontSize: {
+                xs: "11.26px",
+                base: "12px",
+                sm: "14px",
+                "2sm": "15px",
+                md: "16px",
+                "2md": "17px",
+                lg: "18px",
+                xl: "20px",
+                "2xl": "22px",
+                "3xl": "25px",
+                "4xl": "30px",
+                "5xl": "35px",
+                "6xl": "45px",
+                "7xl": "50px",
+                "8xl": "56px",
+                "9xl": "60px",
+                xxl: "65px",
+                "2xxl": "75px",
+            },
+            colors: {
+                darkBlack: "#101828",
+            },
+            screens: {
+                sm: "640px",
+                // => @media (min-width: 640px) { ... }
+
+                md: "768px",
+                // => @media (min-width: 768px) { ... }
+
+                lg: "1024px",
+                // => @media (min-width: 1024px) { ... }
+
+                xl: "1280px",
+                // => @media (min-width: 1280px) { ... }
+
+                "2xl": "1536px",
+                // => @media (min-width: 1536px) { ... }
+
+                "3xl": "1700px",
+                // => @media (min-width: 1700px) { ... }
+            },
+        },
+    },
+    plugins: [],
 };
